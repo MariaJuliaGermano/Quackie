55309af8efa153e13e6922dbce9a57c1cfc9f0ba
Maria Julia Germano <166447580+MariaJuliaGermano@users.noreply.github.com>
2024-10-31 11:16:47 -0300

    Resolve problems, it was an installed_apps problem

--- app_cadastro_usuarios/views.py
+++ app_cadastro_usuarios/views.py
@@ -1,5 +1,15 @@
 from django.shortcuts import render
+from app_cadastro_usuarios.models import Usuario
 
 def home(request):
     return render(request,'usuarios/home.html')
 
+def usuarios(request):
+    novo_usuario = Usuario()
+    novo_usuario.nome = request.POST.get('nome') # Aqui define o campo nome no models usuario, pegando de "nome"
+    novo_usuario.idade = request.POST.get('idade') # Aqui define o campo idade no models usuario, pegando de "idade"
+    novo_usuario.save()
+    usuarios = {
+        'usuarios': Usuario.objects.all() # Colocando em um dicionário todos os objetos do models Usuario que foram cadastrados
+    }
+    return render(request, 'usuarios/usuarios.html', usuarios) # Aqui ele retorna a mudança de página para a página que vai listar os usuários cadastrados e em seguida a própria lista de usuários
--- cadastro_usuarios/urls.py
+++ cadastro_usuarios/urls.py
@@ -1,8 +1,11 @@
+from django.contrib import admin
 from django.urls import path
 from app_cadastro_usuarios import views
 
 urlpatterns = [
-    # rota, view responsável, nome de refência
-    path('',views.home,name='home')
-
+    # rota, view resposável, nome de referência
+    # usuarios.com
+    path('',views.home,name='home'),
+    # usuarios.com/usuarios
+    path('usuarios/',views.usuarios,name='listagem_usuarios')
 ]
