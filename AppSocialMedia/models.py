cf35b2e07bebbf7d1ed1c66237dbd670fb53d844
Maria Julia <mariajuliapmgermano@gmail.com>
2025-06-09 17:08:33 -0300

    fix: ajusta redirecionamento das telas de login e cadastro (close #3)

--- AppSocialMedia/models.py
+++ AppSocialMedia/models.py
@@ -1,7 +1,1 @@
 from django.db import models
-
-class Usuario(models.Model):
-    id_usuario = models.AutoField(primary_key=True)
-    nome = models.TextField(max_length=255, default="Your Name")
-    email = models.EmailField(default="enteryour@email.com")
-    senha = models.TextField(default="default_password")
